7806aaf3a7416471e5c0451a2fb044829ad3b1ea
Ash Ryan <ash@ashryanbeats.com>
2015-08-29 16:13:35 -0400

    Game logic.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,59 @@
 
 angular.module('meowmanApp')
   .controller('MainCtrl', function($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.list = [];
 
-    $http.get('/api/things').then(function(response) {
-      $scope.awesomeThings = response.data;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    $scope.addNote = function() {
+      $scope.list.push(Math.floor(Math.random() * 4));
+      console.log($scope.list);
+    };
 
-    $scope.addThing = function() {
-      if ($scope.newThing === '') {
-        return;
+    $scope.play = function() {
+      var index = 0;
+      var intervalID = setInterval(function(){
+        console.log($scope.list[index]);
+        index++;
+
+        if (index === $scope.list.length) {
+          clearInterval(intervalID);
+          console.log('Interval cleared!');
+        }
+      }, 1000);
+    };
+
+    $scope.pressButton = function(index) {
+      console.log(index);
+      if (index === $scope.list[$scope.userIndex]) {
+        // correct
+        $scope.userIndex++;
+
+        if ($scope.userIndex === $scope.list.length) {
+          //win
+          $scope.userIndex = 0;
+          console.log('Win!');
+        }
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      else {
+        // incorrect
+        $scope.userIndex = 0;
+        console.log('Lose!');
+      }
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    $scope.userIndex = 0;
 
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('thing');
     });
+
+  })
+
+  .filter('range', function() {
+    return function(input, total) {
+      total = parseInt(total);
+      for (var i=0; i<total; i++) {
+        input.push(i);
+      }
+      return input;
+    };
   });
