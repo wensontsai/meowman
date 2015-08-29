ab366e7c0612af25f5b8355e65b64bb502850b97
Sam <sanghun89@gmail.com>
2015-08-29 16:46:21 -0400

    functionial kinda

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meowmanApp')
-  .controller('MainCtrl', function($scope, $http, socket) {
+  .controller('MainCtrl', function($scope, $http, socket, $interval, $timeout) {
     $scope.list = [];
 
     $scope.addNote = function() {
@@ -9,14 +9,25 @@ angular.module('meowmanApp')
       console.log($scope.list);
     };
 
+    $scope.playItem = null;
     $scope.play = function() {
       var index = 0;
-      var intervalID = setInterval(function(){
+      var timeoutId;
+      var intervalID = $interval(function(){
+
+        $scope.playItem = $scope.list[index];
+        timeoutId = $timeout(function() {
+            $scope.playItem = null;
+        }, 200);
         console.log($scope.list[index]);
         index++;
 
         if (index === $scope.list.length) {
-          clearInterval(intervalID);
+          timeoutId.then(function() {
+            $interval.cancel(intervalID);
+          });
+
+          
           console.log('Interval cleared!');
         }
       }, 1000);
@@ -40,6 +51,13 @@ angular.module('meowmanApp')
         console.log('Lose!');
       }
     };
+
+    $scope.cats = [
+        'cat_dj.gif',
+        'cat_piano.jpg',
+        'cat_string.gif',
+        'cat_sunglasses.jpg'
+    ];
 
     $scope.userIndex = 0;
 
